e306a7c6f205c41edd5f0e06d0706888cafbf040
Daniel <dgtutv@gmail.com>
2024-08-04 16:14:45 -0700

    Starting work on gameplay

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Pull valid wordle words
 fetch('https://cors-anywhere.herokuapp.com/https://gist.github.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/valid-wordle-words.txt')
   .then(response => response.text())
   .then(data => {
@@ -5,3 +6,17 @@ fetch('https://cors-anywhere.herokuapp.com/https://gist.github.com/dracos/dd0668
     console.log(wordArray);
   })
   .catch(error => console.error('Error fetching words:', error));
+
+let keys = document.querySelectorAll(".keyboardBox");
+keys.forEach((key) => {
+    key.addEventListener('click', function(event){
+        if(currentBox < 5){
+            let id = event.target.id;
+            boxes[currentBox].innerHTML = id;
+            currentBox++;   
+        }
+    })
+});
+
+let currentBox = 0;
+let boxes = document.querySelectorAll(".letterBox");
