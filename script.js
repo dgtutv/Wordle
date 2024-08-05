51a35548cd47b547e7aacd7fe9ab968928ad4f66
Daniel <dgtutv@gmail.com>
2024-08-04 20:00:55 -0700

    Implemented basic enter functioning

--- script.js
+++ script.js
@@ -12,16 +12,27 @@ keys.forEach((key) => {
     key.addEventListener('click', function(event){
         let id = event.target.id;
         if(id == "DELETE"){
-            if(currentBox > 0){
+            if(currentCol > 0){
                 currentBox--;
+                currentCol--;
                 boxes[currentBox].innerHTML = "";
                 boxes[currentBox].classList.toggle("candidate");
             }
         }
-        else if(currentBox < 5 && id.length == 1){
+        else if(id == "ENTER"){
+            if(currentCol == 5){
+                currentCol = 0;
+                let word = boxes[currentBox-5].innerHTML + boxes[currentBox-4].innerHTML + boxes[currentBox-3].innerHTML + boxes[currentBox-2].innerHTML + boxes[currentBox-1].innerHTML;
+                //Current box has already been incremented 
+                //testWord(word);
+                console.log(word);
+            }
+        }
+        else if(currentCol < 5 && id.length == 1){
             boxes[currentBox].innerHTML = id;
             boxes[currentBox].classList.toggle("candidate");
-            currentBox++;   
+            currentBox++; 
+            currentCol++;  
         }
     })
 });
@@ -31,22 +42,34 @@ document.addEventListener("keydown", (event) => {
     const key = event.key.toUpperCase();
     if (key.length !== 1) {
         if(key == "BACKSPACE"){
-            if(currentBox > 0){
+            if(currentCol > 0){
                 currentBox--;
+                currentCol--;
                 boxes[currentBox].innerHTML = "";
                 boxes[currentBox].classList.toggle("candidate");
+            }
+        }
+        else if(key == "ENTER"){
+            if(currentCol == 5){
+                currentCol = 0;
+                let word = boxes[currentBox-5].innerHTML + boxes[currentBox-4].innerHTML + boxes[currentBox-3].innerHTML + boxes[currentBox-2].innerHTML + boxes[currentBox-1].innerHTML;
+                //Current box has already been incremented 
+                //testWord(word);
+                console.log(word);
             }
         }
         return;
     }
     if(key >= 'A' && key <= 'Z'){
-        if(currentBox < 5){
+        if(currentCol < 5){
             boxes[currentBox].innerHTML = key;
             boxes[currentBox].classList.toggle("candidate");
             currentBox++;
+            currentCol++;
         }
     }
 });
 
 let currentBox = 0;
+let currentCol = 0;
 let boxes = document.querySelectorAll(".letterBox");
